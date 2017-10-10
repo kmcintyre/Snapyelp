305e26e1eb554b56db610081cc969759bbdbd79d
ubuntu <kebin70@gmail.com>
2017-10-10 02:05:36 -0700

    l/g

--- snapyelp/aws/ami.py
+++ snapyelp/aws/ami.py
@@ -9,11 +9,11 @@ def all_instances(region):
 
 @defer.inlineCallbacks
 def region_instance(region_instance_seq):
-    region = region_instance_seq[0]
+    region, instance = region_instance_seq[0], region_instance_seq[1]
     while region[-1:].isalpha():      
         region = region[:-1]
         print 'change to:', region
-    print 'region:', region
+    print 'region:', region, 'instance:', instance
     if region == app_util.app_region:
         print 'connect to:', region
         for image in all_instances(region):
@@ -25,7 +25,7 @@ def region_instance(region_instance_seq):
             except Exception as e:
                 print 'de-register error:', e
         print 'create image:', app_util.app_name
-        ami_response = boto.ec2.connect_to_region(region).create_image(region_instance_seq[1], app_util.app_name)
+        ami_response = boto.ec2.connect_to_region(region).create_image(instance, app_util.app_name)
         print 'ami response:', ami_response.state
         has_tag = False
         while not has_tag:
