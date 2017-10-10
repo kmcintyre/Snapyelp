46278544192384e70e7b17f29807ab4394b8945a
ubuntu <kebin70@gmail.com>
2017-10-09 18:37:09 -0700

    l/g

--- html/init.js
+++
@@ -1,9 +0,0 @@
-define(["jquery", "site", "stream"], function($, site, stream) {		
-	$(document).ready(function() {
-		console.log('create button');
-		$(document.body).append('<button id="randombutton" disabled="disabled">Initializing</button>').one('click', function (e) {			
-			stream.send({'reserve': 'random'});
-			$(e.target).html('Init Reservation')
-		})
-	});		
-});	
--- html/site.js
+++
@@ -1,6 +0,0 @@
-define(['playbook'], function(playbook) {
-	playbook.consume({key:'connection',value:['ws://service.snapyelp.com:8081']}) 		
-	return {		
-		loaded:true
-	}	
-})
