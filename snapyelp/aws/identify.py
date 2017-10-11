4f55c075c8ad9cc5ee6ba05ddd63a0d759118340
Kevin McIntyre <kevin_mcintyre@intuit.com>
2017-10-11 14:46:40 -0700

    l/g

--- snapyelp/aws/identify.py
+++ snapyelp/aws/identify.py
@@ -45,7 +45,7 @@ def selfie():
     print 'region:', region, 'instance id:', instance_id
     if region == app_util.app_region:
         yield defer.maybeDeferred(app_routes.set_cname, 'service.' + app_util.app_name)
-        security_groups = getPage('http://169.254.169.254/latest/meta-data/security-groups')
+        security_groups = yield getPage('http://169.254.169.254/latest/meta-data/security-groups')
         print 'security groups:', security_groups
     for service_name in service_names(region):
         fp = source_service_path(service_name)
