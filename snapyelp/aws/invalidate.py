17cca89f909266463a4fffa251f6077bffb52ec3
Kevin McIntyre <kevin_mcintyre@intuit.com>
2017-10-11 16:47:43 -0700

    l/g

--- snapyelp/aws/invalidate.py
+++ snapyelp/aws/invalidate.py
@@ -11,9 +11,12 @@ def do_invalidate():
                 if ir.status != 'Completed':
                     print 'invalidate request:', ir.id, ir.status
                     exit(1)
-            paths = [res[len(publish.build_dir):] for res in publish.get_publish_list()]
-            print 'paths:', paths
-            c.create_invalidation_request(d.id, paths)
+            paths = [res[len(publish.build_dir):] for res in publish.get_publish_list()]            
+            if paths:
+                print 'invalidate paths:', paths
+                c.create_invalidation_request(d.id, paths)
+            else:
+                print 'invalidate skipped'                
 
 if __name__ == '__main__':    
     do_invalidate()            
