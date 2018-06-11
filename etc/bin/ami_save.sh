e3916d5ea90e4c4ebba7f133f5bf693b461f7725
ubuntu <kebin70@gmail.com>
2018-06-11 13:52:43 -0700

    l/g

--- etc/bin/ami_save.sh
+++ etc/bin/ami_save.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+sudo systemctl stop worker agent websocket
+sudo systemctl disable worker agent websocket
+  
+sudo rm /etc/systemd/system/worker.service
+sudo rm /etc/systemd/system/agent.service
+sudo rm /etc/systemd/system/websocket.service
+		
 sudo swapoff /swapfile
 sudo rm /swapfile
 cd /home/ubuntu/Snapyelp
